faad4bfcc7f38dd63a7fb07fab76beeb09018953
thucy <thucydides.e5k@gmail.com>
2009-04-21 16:37:26 -0400

    drinking beer while coding not good

--- RDevUtils/lib/main.rb
+++ RDevUtils/lib/main.rb
@@ -1,4 +1,19 @@
 # To change this template, choose Tools | Templates
 # and open the template in the editor.
 
-puts "TBWO"
+
+require 'search_source.rb'
+require 'open-uri'
+include SearchSource
+
+results = GoogleSource.search("السلام")
+results.each{|href|
+  open(href) {|page|
+    text = page.read
+
+    Hpricot(text).inner_text.each_line{|line|
+      puts line if line =~ /سلام/
+    }
+  }
+
+}
